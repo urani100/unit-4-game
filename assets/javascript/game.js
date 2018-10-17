6d5f698b08d822f1306d795cdc9f50e5edb9b9e8
M L <ml@Ms-MacBook-Pro.home>
2018-10-17 13:33:55 -0400

    javascript logic 90% completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,6 +14,8 @@ $(document).ready(function(){
     var total = 0;
     var win = 0;
     var loss = 0;
+    var winGame = false;
+    var lossGame = false;
     
    
     //genarate a random master number
@@ -22,25 +24,26 @@ $(document).ready(function(){
          number = Math.floor(Math.random() * 101) + 19;
         $('.mRandomNumber').text(number); 
         return number;
-    }//end of generateRandomNumber
+    }
     
     //generate random number for points
     generatePoints = function(){
         return points = Math.floor(Math.random() * 12) + 1;
     }
     
-    // genarate a random number
-    var randomNumber = generateRandomNumber();
-    
     //generate the points
+    var feedPointArray = function(){
+        for(var i = 0; i<arrPointLength; i++){
+            var point =generatePoints();
+            arrPoints.push(point);
+       }
+    }
     
-    for(var i = 0; i<arrPointLength; i++){
-        var point =generatePoints();
-        arrPoints.push(point);
-   }
+    // call random numbers
+    randomNumber = generateRandomNumber();
+    feedPointArray();
 
     //generate images & assign points to them
-   
    for(var i = 0; i<arrPointLength; i++){
     var body = $('<img>');
     body.addClass('jewels');
@@ -50,26 +53,48 @@ $(document).ready(function(){
    }
 
 //If at one point the score equates the the "master random number" the user wins
-//otherwise the user looses + keep a win score
+//otherwise the user looses + keep a win /loss score
    var compare = function(){
+    //    debugger;
        if(total=== randomNumber){
            alert(total + "  you win")
            win++
            $(".win").text(win);
+           winGame = true;
         }else if(total > randomNumber){
             alert(total + "  You loose");
             loss++;
+            $(".loss").text(loss);
+            lossGame = true;
         }
    }
-   
+
+   var reset = function(){
+    //    debugger;
+       arrPoints = [];
+       score = 0;
+       randomNumber = 0;
+       points = 0;
+       total = 0;
+       winGame = false;
+       lossGame = false;
+       randomNumber = generateRandomNumber();
+       feedPointArray();
+       $('.score').empty();
+   }
+
    //associate clicks to points add points... evaluate points 
    $('.jewels').on("click", function(){
+       if(winGame || lossGame){
+        reset();
+       }
        var value = ($(this).attr('data-value'));
        total+= parseInt(value);
        $('.score').text(total);
         compare();
     })
 
-//Reset the game  after a win or a loss
+    
 
+   
 })
