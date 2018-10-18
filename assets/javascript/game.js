790bf715a192d09f6c04a913948e9195bd4665c8
M L <ml@Ms-MacBook-Pro.home>
2018-10-18 12:02:15 -0400

    adjusting code to be within an object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,93 +6,94 @@ var game={
     'assets/images/hedgehog.jpg',
     'assets/images/squirrel.jpg',
     'assets/images/rabbit.jpg'],
-    arrPoints = [], // var arrPoints = new Array(4) does not work... first element is "empty"
-    arrPointLength = 4,
-    score = 0,
-    randomNumber = 0,
-    points = 0,
-var total = 0;
-var win = 0;
-var loss = 0;
-var winGame = false;
-var lossGame = false;
+    arrPoints: [], // var arrPoints = new Array(4) does not work... first element is "empty"
+    arrPointLength : 4,
+    score: 0,
+    randomNumber: 0,
+    points: 0,
+    total: 0,
+    win: 0,
+    loss: 0,
+    winGame: false,
+    lossGame: false,
 
-}
-   
-    
-   
-    //genarate a random master number
-    generateRandomNumber =  function(){
+    //genarates a the main random number
+    generateRandomNumber:  function(){
         var number = 0;
          number = Math.floor(Math.random() * 101) + 19;
         $('.mRandomNumber').text(number); 
         return number;
-    }
+    },
+
+    //generates random number for points
+    generatePoints : function(){
+        return this.points = Math.floor(Math.random() * 12) + 1;
+    },
     
-    //generate random number for points
-    generatePoints = function(){
-        return points = Math.floor(Math.random() * 12) + 1;
-    }
-    
-    //generate the points
-    var feedPointArray = function(){
-        for(var i = 0; i<arrPointLength; i++){
-            var point =generatePoints();
-            arrPoints.push(point);
+    //generate the points and pushes them in an array
+    feedPointArray : function(){
+        for(var i = 0; i<this.arrPointLength; i++){
+            var point = this.generatePoints();
+            this.arrPoints.push(point);
        }
-    }
-    
-    // call random numbers
-    randomNumber = generateRandomNumber();
-    feedPointArray();
+    },
 
-    //generate images & assign points to them
-   for(var i = 0; i<arrPointLength; i++){
-    var body = $('<img>');
-    body.addClass('jewels');
-    body.attr('src', images[i]);
-    body.attr('data-value', arrPoints[i]);
-    $('.points').append(body).css('display', 'inline-block');
-   }
-
-//If at one point the score equates the the "master random number" the user wins
-//otherwise the user looses + keep a win /loss score
-   var compare = function(){
-    //    debugger;
-       if(total=== randomNumber){
-           win++
-           $(".win").text(win);
-           winGame = true;
-        }else if(total > randomNumber){
-            loss++;
-            $(".loss").text(loss);
-            lossGame = true;
+    //If at one point the score equates the the random number the user wins
+    //otherwise the user looses + keep a win /loss score
+    compare: function(){
+        if(this.total=== this.randomNumber){
+           this.win++
+           $(".win").text(this.win);
+           this.winGame = true;
+        }else if(this.total > this.randomNumber){
+            this.loss++;
+            $(".loss").text(this.loss);
+            this.lossGame = true;
         }
-   }
+    },
+    
+    initialSetUp: function(){
+    // call random numbers
+    this.randomNumber= this.generateRandomNumber();
+    //set up array
+    this.feedPointArray();
+    //generate images & assign points to them
+    for(var i = 0; i<this.arrPointLength; i++){
+        var body = $('<img>');
+        body.addClass('jewels');
+        body.attr('src', this.images[i]);
+        body.attr('data-value', arrPoints[i]);
+        $('.points').append(body).css('display', 'inline-block');
+       }
+    },
+    // resets game to it original state
+    reset : function(){
+        this.arrPoints = [];
+        this.score = 0;
+        this.randomNumber = 0;
+        this.points = 0;
+        this.total = 0;
+        this.winGame = false;
+        this.lossGame = false;
+        this.randomNumber = generateRandomNumber();
+        this.feedPointArray();
+        $('.score').empty();
+    }
 
-   var reset = function(){
-    //    debugger;
-       arrPoints = [];
-       score = 0;
-       randomNumber = 0;
-       points = 0;
-       total = 0;
-       winGame = false;
-       lossGame = false;
-       randomNumber = generateRandomNumber();
-       feedPointArray();
-       $('.score').empty();
-   }
+}//end of object
+   
+    
+game.initialSetUp();
 
    //associate clicks to points add points... evaluate points 
    $('.jewels').on("click", function(){
-       if(winGame || lossGame){
-        reset();
+       if(game.winGame || game.lossGame){
+        game.reset();
        }
        var value = ($(this).attr('data-value'));
-       total+= parseInt(value);
-       $('.score').text(total);
-        compare();
+       game.total+= parseInt(value);
+       $('.score').text(game.total);
+        game.compare();
     })
 
-})
+})// end of document.resdy
